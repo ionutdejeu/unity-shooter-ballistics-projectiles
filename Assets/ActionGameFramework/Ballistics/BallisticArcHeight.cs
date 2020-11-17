b7653140b0f34b98b3459c80e0a95ff88ac2e870
ionutdejeu <ionut.dejeu@gmail.com>
2020-11-17 19:07:17 +0200

    Intial commit. Basic projectil classes

---
+++ Assets/ActionGameFramework/Ballistics/BallisticArcHeight.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.ActionGameFramework.Ballistics
+{
+
+	/// <summary>
+	/// Ballistic arc calculation priorities/preferences.
+	/// </summary>
+	public enum BallisticArcHeight
+	{
+		/// <summary>
+		/// High "underarm" arc
+		/// </summary>
+		UseHigh,
+
+		/// <summary>
+		/// Low "overarm" arc
+		/// </summary>
+		UseLow,
+
+		/// <summary>
+		/// Use high arc if valid, fall back to low if possible.
+		/// </summary>
+		PreferHigh,
+
+		/// <summary>
+		/// Use low arc if valid, fall back to high if possible.
+		/// </summary>
+		PreferLow
+	}
+}
