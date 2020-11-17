18152a97ee9f9527d934f7600cf8c49e0b12726a
ionutdejeu <ionut.dejeu@gmail.com>
2020-11-17 22:45:54 +0200

    Ballistics launcher copy from action game framework

---
+++ Assets/ActionGameFramework/Launcher/BallisticLauncher.cs
@@ -0,0 +1,64 @@
+﻿using Assets.ActionGameFramework.Ballistics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.ActionGameFramework.Launcher
+{
+	/// <summary>
+	/// Implementation of the tower launcher for Ballistic Projectiles
+	/// </summary>
+	public class BallisticLauncher : Launcher
+	{
+		/// <summary>
+		/// The particle system used for providing launch feedback
+		/// </summary>
+		public ParticleSystem fireParticleSystem;
+
+		/// <summary>
+		/// Launches a single projectile at a single enemy from a single firing point
+		/// </summary>
+		/// <param name="enemy">
+		/// The enemy to target
+		/// </param>
+		/// <param name="projectile">
+		/// The projectile to attack
+		/// </param>
+		/// <param name="firingPoint">
+		/// The point to fire from
+		/// </param>
+		public override void Launch(GameObject enemy, GameObject projectile, Transform firingPoint)
+		{
+			Vector3 startPosition = firingPoint.position;
+			var ballisticProjectile = projectile.GetComponent<BallisticProjectile>();
+			if (ballisticProjectile == null)
+			{
+				Debug.LogError("No ballistic projectile attached to projectile");
+				DestroyImmediate(projectile);
+				return;
+			}
+			Vector3 targetPoint;
+			if (ballisticProjectile.fireMode == BallisticFireMode.UseLaunchSpeed)
+			{
+				// use speed
+				targetPoint = BallisticHelper.CalculateBallisticLeadingTargetPointWithSpeed(
+					startPosition,
+					enemy.transform.position, Vector3.one,
+					ballisticProjectile.startSpeed, ballisticProjectile.arcPreference, Physics.gravity.y, 4);
+			}
+			else
+			{
+				// use angle
+				targetPoint = BallisticHelper.CalculateBallisticLeadingTargetPointWithAngle(
+					startPosition,
+					enemy.transform.position, Vector3.one, ballisticProjectile.firingAngle,
+					ballisticProjectile.arcPreference, Physics.gravity.y, 4);
+			}
+			ballisticProjectile.FireAtPoint(startPosition, targetPoint);
+			PlayParticles(fireParticleSystem, startPosition, targetPoint);
+		}
+	}
+}
---
+++ Assets/ActionGameFramework/Launcher/ILauncher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.ActionGameFramework.Launcher
+{
+	/// <summary>
+	/// A class that allows the TowerConfiguration to delegate
+	/// different firing logic this component
+	/// </summary>
+	public interface ILauncher
+	{
+		/// <summary>
+		/// The method for crafting the firing logic for the tower
+		/// </summary>
+		/// <param name="enemy">
+		/// The enemy that the tower is targeting
+		/// </param>
+		/// <param name="attack">
+		/// The projectile component used to attack the enemy
+		/// </param>
+		/// <param name="firingPoint"></param>
+		void Launch(GameObject enemy, GameObject attack, Transform firingPoint);
+
+		/// <summary>
+		/// The method for crafting the firing logic for the tower
+		/// </summary>
+		/// <param name="enemy">
+		/// The enemy that the tower is targeting
+		/// </param>
+		/// <param name="attack">
+		/// The projectile component used to attack the enemy
+		/// </param>
+		/// <param name="firingPoints">
+		/// A list of firing points to fire from
+		/// </param>
+		void Launch(GameObject enemy, GameObject attack, Transform[] firingPoints);
+
+		/// <summary>
+		/// The method for crafting firing logic at multiple enemies
+		/// </summary>
+		/// <param name="enemies">
+		/// The collection of enemies to attack
+		/// </param>
+		/// <param name="attack">
+		/// The projectile component used to attack the enemy
+		/// </param>
+		/// <param name="firingPoints"></param>
+		void Launch(List<GameObject> enemies, GameObject attack, Transform[] firingPoints);
+	}
+}
