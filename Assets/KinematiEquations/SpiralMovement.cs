e0b450e291bce93598656b86c46369f1b84afc3f
ionutdejeu <ionut.dejeu@gmail.com>
2020-12-06 11:27:44 +0200

    Cleanup code

--- Assets/KinematiEquations/SpiralMovement.cs
+++ Assets/KinematiEquations/SpiralMovement.cs
@@ -11,34 +11,24 @@ public class SpiralMovement : MonoBehaviour
     public int intialAngle = 30;
 
 
-    float flightTime = 5f;
-    Vector3 speed;
-
     Quaternion rotationAtSpeed;
     
     public float StartingRadius = 10f;
     public float EndRadius = 0f;
     Vector3 movementDirection;
-    Quaternion movementDirectionRotation;
 
     private void Start()
     {
-        movementDirection = (target.position - start.position);
-        movementDirectionRotation = Quaternion.LookRotation(movementDirection);
-        
+        movementDirection = (target.position - start.position);        
         visual.positionCount = lineSegmentCount+1;
     }
     private void Update()
     {
         rotationAtSpeed = Quaternion.AngleAxis(intialAngle, movementDirection);
-        Visualize(speed, target.position);
-         
-
-        //var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
-        //transform.position = _centre + offset;
+        Visualize();
+        
     }
-    //added final position argument to draw the last line node to the actual target
-    void Visualize(Vector3 speed, Vector3 targetPos)
+    void Visualize()
     {
         for (int i = 0; i <=lineSegmentCount; i++)
         {
@@ -52,14 +42,6 @@ public class SpiralMovement : MonoBehaviour
             Debug.DrawLine(pos, finalPos, Color.red);
             visual.SetPosition(i, pos+rPovit);
         }
-
-        //visual.SetPosition(lineSegmentCount, targetPos);
     }
-
-    public Vector3 RotatePointAroundPivotWithRadius(Vector3 dir, Vector3 position, Quaternion rot)
-    {
-        return position + (rot * dir); 
-    }
-
  
 }
